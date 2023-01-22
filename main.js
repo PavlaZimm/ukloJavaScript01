1bc12bf2983a6afbd46f8db7fa7b991a72567e2b
Pavla Zimm <zimmermannovap@gmail.com>
2023-01-22 19:59:59 +0100

    1ukol

---
+++ main.js
@@ -0,0 +1,18 @@
+// tady je místo pro náš program
+
+/*
+
+let nadpis= document.querySelector('h1');
+
+console.log( nadpis )
+
+
+*/
+
+document.querySelector("h1").style.color="blue";
+document.querySelector("p").style.fontSize="26px";
+
+let kocka = document.querySelector('.kocka');
+kocka.src = 'pes.jpg';
+
+
