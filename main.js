76e10dbdee66415f2fac027c927b4b8a320197dc
Pavla Zimm <zimmermannovap@gmail.com>
2023-01-24 14:37:36 +0100

    hotovo

--- main.js
+++ main.js
@@ -11,8 +11,15 @@ console.log( nadpis )
 
 document.querySelector("h1").style.color="blue";
 document.querySelector("p").style.fontSize="26px";
+document.querySelector(".kocka").style.marginLeft = "50px";
+document.querySelector(".ctverecek").style.marginLeft = "150px";
+
 
 let kocka = document.querySelector('.kocka');
 kocka.src = 'pes.jpg';
 
+function posunKostku(){
+    let ctverecek = document.querySelector('.ctverecek');
+    ctverecek.style.marginLeft = "50px";
+}
 
